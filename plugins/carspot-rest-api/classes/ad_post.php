5bf86ea5862bd75574613faee56162f13c73d578
rockowtham <kumar.gowtham@gmail.com>
2020-08-28 21:14:21 +0530

    added add detail

--- plugins/carspot-rest-api/classes/ad_post.php
+++ plugins/carspot-rest-api/classes/ad_post.php
@@ -666,8 +666,9 @@ if (!function_exists('carspotAPI_post_ad_post')) {
 			} else {
 				$pid	=	$json_data['ad_id'];
 			}
+			echo $ad_categories_string = parse_json(json_decode($ad_categories,true));exit;
 			update_post_meta($pid, '_carspot_ad_categories', $ad_categories);
-			update_post_meta($pid, '_carspot_ad_categories_string', parse_json(json_decode($ad_categories,true)));
+			update_post_meta($pid, '_carspot_ad_categories_string', $ad_categories_string);
 			// echo $pid;exit;
 			delete_user_meta($user_id, 'ad_in_progress');
 			// if (!is_super_admin($user_id)) {
