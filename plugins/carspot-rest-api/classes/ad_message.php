2c0f9dc76441f9b941fefef6953e2c512c99456c
rockowtham <kumar.gowtham@gmail.com>
2020-09-26 14:46:20 +0530

    added

--- plugins/carspot-rest-api/classes/ad_message.php
+++ plugins/carspot-rest-api/classes/ad_message.php
@@ -296,7 +296,7 @@ if (!function_exists('carspotAPI_messages_chat_get'))
 			$push 	  = new carspotAPI_Push();
 			$payload = array();
 			$title = "New Message";
-			$message = "For Your Add ".$data['ad_title'];
+			$message = "You Have got a new Message From ".$page_title;
 			$setMsgTopic = "New Message";
 			$push->setTitle($title);
 			$push->setMessage($message);
